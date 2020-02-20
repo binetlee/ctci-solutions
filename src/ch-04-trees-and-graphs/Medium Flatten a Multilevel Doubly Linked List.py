92d873070ef40d8efd6e558c546b92174c5368f5
bcl04ae <binet_c_lee@homedepot.com>
2020-02-20 14:26:14 -0600

    time to work on DFS

---
+++ src/ch-04-trees-and-graphs/Medium Flatten a Multilevel Doubly Linked List.py
@@ -0,0 +1,13 @@
+##https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+##You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+
+##Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
+from collections import *
+from typing import List
+
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+
+d = Solution().twoSum
+print(d([2, 7, 11, 15],17))
